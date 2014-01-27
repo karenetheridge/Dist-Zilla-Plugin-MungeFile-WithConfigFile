e3e2217cf3f961b1d5f8740394eb0dfb88a0d40b
Karen Etheridge <ether@cpan.org>
2014-01-26 17:29:20 -0800

    [MungeFile::WithData] -> [MungeFile::WithDataSection]

--- lib/Dist/Zilla/Plugin/MungeFile/WithConfigFile.pm
+++ lib/Dist/Zilla/Plugin/MungeFile/WithConfigFile.pm
@@ -197,7 +197,7 @@ I am also usually active on irc, as 'ether' at C<irc.perl.org>.
 
 * L<Dist::Zilla::Plugin::Substitute>
 * L<Dist::Zilla::Plugin::GatherDir::Template>
-* L<Dist::Zilla::Plugin::MungeFile::WithData>
+* L<Dist::Zilla::Plugin::MungeFile::WithDataSection>
 
 =end :list
 
