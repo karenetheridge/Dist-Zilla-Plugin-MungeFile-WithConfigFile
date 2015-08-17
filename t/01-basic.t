karenetheridge/Dist-Zilla-Plugin-MungeFile-WithData

properly test metaconfig data

t/01-basic.t
@@ -5,6 +5,7 @@ use Test::More;
 use if $ENV{AUTHOR_TESTING}, 'Test::Warnings';
 use Test::DZil;
 use Path::Tiny;
+use Test::Deep;
 
 my $tzil = Builder->from_config(
     { dist_root => 't/does_not_exist' },
@@ -12,6 +13,7 @@ my $tzil = Builder->from_config(
         add_files => {
             path(qw(source dist.ini)) => simple_ini(
                 [ GatherDir => ],
+                [ MetaConfig => ],
                 [ 'MungeFile::WithDataSection' => { finder => ':MainModule' } ],
             ),
             'source/lib/Module.pm' => <<'MODULE'
@@ -56,6 +58,28 @@ This is content that should not be in the DATA section.
 NEW_MODULE
     'module content is transformed',
 );
+
+cmp_deeply(
+    $tzil->distmeta,
+    superhashof({
+        x_Dist_Zilla => superhashof({
+            plugins => supersetof(
+                {
+                    class => 'Dist::Zilla::Plugin::MungeFile::WithDataSection',
+                    config => {
+                        'Dist::Zilla::Plugin::MungeFile::WithDataSection' => {
+                            finder => [ ':MainModule' ],
+                            files => [ ],
+                        },
+                    },
+                    name => 'MungeFile::WithDataSection',
+                    version => ignore,
+                },
+            ),
+        }),
+    }),
+    'distmeta is correct',
+) or diag 'got distmeta: ', explain $tzil->distmeta;
 
 diag 'got log messages: ', explain $tzil->log_messages
     if not Test::Builder->new->is_passing;

t/02-file.t
@@ -5,6 +5,7 @@ use Test::More;
 use if $ENV{AUTHOR_TESTING}, 'Test::Warnings';
 use Test::DZil;
 use Path::Tiny;
+use Test::Deep;
 
 my $tzil = Builder->from_config(
     { dist_root => 't/does_not_exist' },
@@ -12,6 +13,7 @@ my $tzil = Builder->from_config(
         add_files => {
             path(qw(source dist.ini)) => simple_ini(
                 [ GatherDir => ],
+                [ MetaConfig => ],
                 [ 'MungeFile::WithDataSection' => { file => ['lib/Module.pm'] } ],
             ),
             'source/lib/Module.pm' => <<'MODULE'
@@ -52,6 +54,28 @@ pony
 NEW_MODULE
     'module content is transformed',
 );
+
+cmp_deeply(
+    $tzil->distmeta,
+    superhashof({
+        x_Dist_Zilla => superhashof({
+            plugins => supersetof(
+                {
+                    class => 'Dist::Zilla::Plugin::MungeFile::WithDataSection',
+                    config => {
+                        'Dist::Zilla::Plugin::MungeFile::WithDataSection' => {
+                            finder => [ ],
+                            files => [ 'lib/Module.pm' ],
+                        },
+                    },
+                    name => 'MungeFile::WithDataSection',
+                    version => ignore,
+                },
+            ),
+        }),
+    }),
+    'distmeta is correct',
+) or diag 'got distmeta: ', explain $tzil->distmeta;
 
 diag 'got log messages: ', explain $tzil->log_messages
     if not Test::Builder->new->is_passing;

karenetheridge/Dist-Zilla-Plugin-MungeFile-WithConfigFile

t/01-basic.t
@@ -5,6 +5,7 @@ use Test::More;
 use if $ENV{AUTHOR_TESTING}, 'Test::Warnings';
 use Test::DZil;
 use Config::Tiny;   # to read .ini files
+use Test::Deep;
 
 my $tzil = Builder->from_config(
     { dist_root => 't/does_not_exist' },
@@ -12,6 +13,7 @@ my $tzil = Builder->from_config(
         add_files => {
             'source/dist.ini' => simple_ini(
                 [ GatherDir => ],
+                [ MetaConfig => ],
                 [ 'MungeFile::WithConfigFile' => { finder => ':MainModule', configfile => 'config.ini', house => 'maison' } ],
             ),
             'source/config.ini' => <<'CONFIG',
@@ -53,5 +55,29 @@ my $maison = 'my castle';
 NEW_MODULE
     'module content is transformed',
 );
+
+cmp_deeply(
+    $tzil->distmeta,
+    superhashof({
+        x_Dist_Zilla => superhashof({
+            plugins => supersetof(
+                {
+                    class => 'Dist::Zilla::Plugin::MungeFile::WithConfigFile',
+                    config => {
+                        'Dist::Zilla::Plugin::MungeFile::WithConfigFile' => {
+                            finder => [ ':MainModule' ],
+                            files => [ ],
+                            configfile => 'config.ini',
+                            house => 'maison',
+                        },
+                    },
+                    name => 'MungeFile::WithConfigFile',
+                    version => Dist::Zilla::Plugin::MungeFile::WithConfigFile->VERSION,
+                },
+            ),
+        }),
+    }),
+    'distmeta is correct',
+) or diag 'got distmeta: ', explain $tzil->distmeta;
 
 done_testing;

t/02-file.t
@@ -5,6 +5,7 @@ use Test::More;
 use if $ENV{AUTHOR_TESTING}, 'Test::Warnings';
 use Test::DZil;
 use Config::Tiny;   # to read .ini files
+use Test::Deep;
 
 my $tzil = Builder->from_config(
     { dist_root => 't/does_not_exist' },
@@ -12,6 +13,7 @@ my $tzil = Builder->from_config(
         add_files => {
             'source/dist.ini' => simple_ini(
                 [ GatherDir => ],
+                [ MetaConfig => ],
                 [ 'MungeFile::WithConfigFile' => { file => ['lib/Module.pm'], configfile => 'config.ini' } ],
             ),
             'source/config.ini' => <<'CONFIG',
@@ -51,5 +53,28 @@ And that's just great!\n";
 NEW_MODULE
     'module content is transformed',
 );
+
+cmp_deeply(
+    $tzil->distmeta,
+    superhashof({
+        x_Dist_Zilla => superhashof({
+            plugins => supersetof(
+                {
+                    class => 'Dist::Zilla::Plugin::MungeFile::WithConfigFile',
+                    config => {
+                        'Dist::Zilla::Plugin::MungeFile::WithConfigFile' => {
+                            finder => [ ],
+                            files => [ 'lib/Module.pm' ],
+                            configfile => 'config.ini',
+                        },
+                    },
+                    name => 'MungeFile::WithConfigFile',
+                    version => Dist::Zilla::Plugin::MungeFile::WithConfigFile->VERSION,
+                },
+            ),
+        }),
+    }),
+    'distmeta is correct',
+) or diag 'got distmeta: ', explain $tzil->distmeta;
 
 done_testing;